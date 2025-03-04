4c33568ea07af9d4704552e33c1aa8812ed7f71e
TriTech <136884744+TriTechX@users.noreply.github.com>
2025-03-04 12:52:34 +0000

    Add files via upload

---
+++ core2/colours (2).py
@@ -0,0 +1,35 @@
+def black():
+    return "\033[30m"
+
+def red():
+    return "\033[31m"
+
+def green():
+    return "\033[92m"
+
+def yellow():
+    return "\033[33m"
+
+def blue():
+    return "\033[34m"
+
+def magenta():
+    return "\033[35m"
+
+def cyan():
+    return "\033[96m"
+
+def white():
+    return "\033[37m"
+
+def reset():
+    return "\033[0m"
+
+def grey():
+    return "\033[90m"
+
+def bold():
+    return "\033[1m"
+
+def dgreenBg():
+    return "\033[42m"
